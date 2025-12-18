d3c12008d88fe5e53d55908d5d1beeeed8ee4c8e
Zharylkassynov <artur.zharylkassynov@narxoz.kz>
2025-12-18 14:27:32 +0500

    fix: fixes during testing

--- bot.py
+++ bot.py
@@ -1,13 +1,38 @@
 import asyncio
+import os
 from aiogram import Bot, Dispatcher
+from aiohttp import web
+
 from config import BOT_TOKEN
 from handlers import router
+
+
+async def healthcheck(request):
+    return web.Response(text="OK")
+
+
+async def start_web_server():
+    app = web.Application()
+    app.router.add_get("/", healthcheck)
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+
+    port = int(os.environ.get("PORT", 10000))
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
+
 
 async def main():
     bot = Bot(BOT_TOKEN)
     dp = Dispatcher()
     dp.include_router(router)
-    await dp.start_polling(bot)
+
+    await asyncio.gather(
+        dp.start_polling(bot),
+        start_web_server()
+    )
+
 
 if __name__ == "__main__":
     asyncio.run(main())
